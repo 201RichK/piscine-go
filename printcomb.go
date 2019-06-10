a595a73935ec879cfddd0818001c9e31a910af7f
RichK <amosamospascal@yahoo.com>
2019-06-10 03:29:02 +0100

    printComb

---
+++ printcomb.go
@@ -0,0 +1,28 @@
+package piscine
+
+import "fmt"
+
+func PrintComb() {
+	a := 0
+	b := 0
+	c := 0
+	for a <= 7 {
+		for b <= 8 {
+			for c <= 9 {
+
+				if a < b && b < c {
+					if a != 7 || b != 8 || c != 9 {
+						fmt.PrintRune(',')
+						fmt.PrintRune(' ')
+					}
+				}
+
+				c++
+			}
+			c = 0
+			b++
+		}
+		b = 0
+		a++
+	}
+}
