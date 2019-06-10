58f4abc9f362793e45a1e9edf498f6e45522c4e5
RichK <amosamospascal@yahoo.com>
2019-06-10 13:34:20 +0100

    printComb2

--- printComb2.go
+++ printComb2.go
@@ -17,7 +17,7 @@ func PrintComb2() {
 	for a := 0; a < 10; a++ {
 		for b := 0; b < 10; b++ {
 			for c := 0; c < 10; c++ {
-				for d := 0; d < count; d++ {
+				for d := 0; d < 10; d++ {
 
 					nb1 := strconv.Itoa(a) + strconv.Itoa(b)
 					nb2 := strconv.Itoa(c) + strconv.Itoa(d)
