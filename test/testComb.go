d73c0288f29b949ababd1b7ea1a6fd6bd5454e04
RichK <amosamospascal@yahoo.com>
2019-06-12 10:54:32 +0100

    basic atoi2

--- test/testComb.go
+++ test/testComb.go
@@ -1,44 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
-
-func PrintComb() {
-
-	var b int = 0
-	var c int = 0
-
-	//var sep string = ", "
-
-	for b < 10 {
-		for c < 10 {
-
-			if b == c {
-
-			} else {
-				if b == 9 && c == 8 {
-					fmt.Printf("%d%d", b, c)
-				} else {
-					fmt.Printf("%d%d", b, c)
-					//fmt.Println(a, b, c)
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				}
-			}
-
-			c++
-		}
-		c = 0
-		b++
-	}
-
-	z01.PrintRune('\n')
-
-}
+import "fmt"
 
 func main() {
 
-	PrintComb()
+	s := "Hello World!"
+	//s = StrRev(s)
+	fmt.Println(s)
+	a := len([]rune(s))
+	fmt.Println(a)
+
+	for i := a; i > 0; i-- {
+		fmt.Print(s[i])
+	}
 
 }
