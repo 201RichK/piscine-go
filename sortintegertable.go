8f26d0e7173584ca6be257b056ae526c8c416418
RichK <amosamospascal@yahoo.com>
2019-06-12 11:23:57 +0000

    sort int table

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,19 +1,9 @@
-package main
+package piscine
 
-import (
-	"fmt"
-	"sort"
-)
 
 /*https://golang.org/pkg/sort/*/
 func SortIntegerTable(table []int) {
 	//	s := []int{5, 2, 6, 3, 1, 4} // unsorted
 	sort.Ints(table)
 	//fmt.Println(s)
-}
-
-func main() {
-	s := []int{5, 4, 3, 2, 1, 0}
-	SortIntegerTable(s)
-	fmt.Println(s)
 }
