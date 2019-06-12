722bd88182132ad627d6fabecb8d8fc41067fc1f
RichK <amosamospascal@yahoo.com>
2019-06-12 22:30:31 +0000

    recursif factorial func

---
+++ recursivefactorial.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "math"
+
+func RecursiveFactorial(nb int) int {
+	result := 1
+
+	if nb >= 1 {
+		result = nb * RecursiveFactorial(nb-1)
+
+		if result > math.MaxInt32 {
+			return 0
+		}
+
+	} else if nb == 0 || nb == 1 {
+		result = 1
+	} else {
+		result = 0
+	}
+
+	return result
+}
