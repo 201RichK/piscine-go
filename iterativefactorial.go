5f16684ed2b06437f12826b2ffe4254922e2d329
RichK <amosamospascal@yahoo.com>
2019-06-12 20:21:58 +0000

    iteractive factoriel func

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,7 +4,7 @@ import "math"
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	if nb > 0 {
+	if nb >= 0 {
 		for i := 1; i < nb+1; i++ {
 			result = result * i
 			if result > math.MaxInt32 {
