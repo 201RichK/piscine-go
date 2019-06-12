0741ac8a104c4c911e7bfb34f9d24f9565fe521c
RichK <amosamospascal@yahoo.com>
2019-06-12 11:03:30 +0100

    basic atoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -5,6 +5,8 @@ package piscine
 
 
 	byte is an alias for uint8 and is equivalent to uint8 in all ways. It is used, by convention, to distinguish byte values from 8-bit unsigned integer values.
+
+	https://golang.org/src/bytes/example_test.goz
 */
 
 func BasicAtoi2(s string) int {
