fc41a550a6969940d3f818e3e7961499c3800e4d
RichK <amosamospascal@yahoo.com>
2019-06-13 13:27:15 +0000

    is prime

--- isprime.go
+++ isprime.go
@@ -1,7 +1,5 @@
 package piscine
 
-import "math"
-
 func IsPrime(nb int) bool {
 	a := 2 + (2 * (RecursiveFactorial(nb - 1)) % nb)
 
@@ -11,16 +9,4 @@ func IsPrime(nb int) bool {
 	default:
 		return true
 	}
-}
-func RecursiveFactorial(nb int) int {
-	if nb > 1 {
-		if nb > math.MaxInt32 {
-			return 0
-		}
-		return nb * RecursiveFactorial(nb-1)
-	} else if nb == 0 || nb == 1 {
-		return 1
-	}
-
-	return 0
 }
