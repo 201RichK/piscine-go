9aac72a1cb6554d08813249eaa6c656d7ebe514f
RichK <amosamospascal@yahoo.com>
2019-06-13 13:39:20 +0000

    is prime

--- isprime.go
+++ isprime.go
@@ -1,12 +1,16 @@
 package piscine
 
-func IsPrime(nb int) bool {
-	a := 2 + (2 * (RecursiveFactorial(nb - 1)) % nb)
+import "math"
 
-	switch a {
-	case 2:
-		return false
-	default:
-		return true
+func IsPrime(nb int) bool {
+	//a := 2 + (2 * (RecursiveFactorial(nb - 1)) % nb)
+	var a bool
+	for i := 2; i <= int(math.Floor(float64(nb)/2)); i++ {
+		if nb%i == 0 {
+			a = false
+		} else {
+			a = true
+		}
 	}
+	return a
 }
