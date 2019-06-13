61620ff8917d21f4ceb3515e04708fb79b53078b
RichK <amosamospascal@yahoo.com>
2019-06-13 15:43:07 +0000

    find next prime

--- findnextprime.go
+++ findnextprime.go
@@ -1,11 +1,14 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	for i := nb; i >= nb; i++ {
-		if IsPrime(i) {
-			return i
-			break
+	nextprime := nb - 1
+	i := nb + 1
+	for i > nb {
+		nextprime++
+		if premier(nextprime) {
+			return nextprime
 		}
+		i++
 	}
-	return 0
+	return nextprime
 }
