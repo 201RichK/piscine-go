d213b0d48f86085640fccd461ba84d52be0acb12
RichK <amosamospascal@yahoo.com>
2019-06-14 16:33:13 +0000

    virify is somme string is alphanumerique

---
+++ isalpha.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsAlpha(str string) bool {
+
+	var type bool 
+
+	for i := 0; i < len(str); i++ {
+		if 'a' <= str[i] && str[i]  <= 'z' || 'A' <= str[i] && str[i] <= 'Z' || '0' <= str[i] && <= '9' {
+			type = true
+		}else{
+			type = false
+		}
+	}
+	return type
+}
