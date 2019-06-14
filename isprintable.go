4b64ec0a2187c9437fb61cff7a0029792099caa7
RichK <amosamospascal@yahoo.com>
2019-06-14 21:30:28 +0000

    isprintble

--- isprintable.go
+++ isprintable.go
@@ -1,13 +1,11 @@
 package piscine
 
 func IsPrintable(str string) bool {
-	if str == "hello\n" {
-		return true
-	}
+
 	b := []byte(str)
 	var t bool
 	for i := 0; i < len(b); i++ {
-		if 65 <= str[i] && str[i] <= 90 || 97 <= str[i] && str[i] <= 122 || 47 <= str[i] && str[i] <= 57 || str[i] == 33 {
+		if 65 <= str[i] && str[i] <= 90 || 97 <= str[i] && str[i] <= 122 || 47 <= str[i] && str[i] <= 57 || str[i] == 33 || str[i] == 10 {
 			return true
 		} else {
 			t = false
