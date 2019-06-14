05c9b234afe70f53512ef8b113621e74d820dd5c
RichK <amosamospascal@yahoo.com>
2019-06-14 18:08:22 +0000

    isprintable

---
+++ isprintable.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IsPrintable(str string) bool {
+	var uper bool
+	for i := 0; i < len(str); i++ {
+		if 'A' <= str[i] && str[i] <= 'Z' {
+			uper = true
+		} else {
+			return false
+		}
+	}
+	return uper
+}
