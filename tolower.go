504aec3e509dd33c931dbd253b80cf349c89b0e5
RichK <amosamospascal@yahoo.com>
2019-06-14 15:03:15 +0000

    to lower

--- tolower.go
+++ tolower.go
@@ -1,35 +1,7 @@
 package piscine
 
-import (
-	"unicode"
-	"unicode/utf8"
-)
+import "strings"
 
 func ToLower(s string) string {
-	isASCII, hasUpper := true, false
-	for i := 0; i < len(s); i++ {
-		c := s[i]
-		if c >= utf8.RuneSelf {
-			isASCII = false
-			break
-		}
-		hasUpper = hasUpper || (c >= 'A' && c <= 'Z')
-	}
-
-	if isASCII { // optimize for ASCII-only strings.
-		if !hasUpper {
-			return s
-		}
-		var b Builder
-		b.Grow(len(s))
-		for i := 0; i < len(s); i++ {
-			c := s[i]
-			if c >= 'A' && c <= 'Z' {
-				c += 'a' - 'A'
-			}
-			b.WriteByte(c)
-		}
-		return b.String()
-	}
-	return Map(unicode.ToLower, s)
+	return strings.ToLower(s)
 }
