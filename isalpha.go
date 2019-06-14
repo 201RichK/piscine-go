bec819167ec20dc53e72b84b6897853f019f872f
RichK <amosamospascal@yahoo.com>
2019-06-14 16:39:00 +0000

    virify is somme string is alphanumerique

--- isalpha.go
+++ isalpha.go
@@ -2,14 +2,14 @@ package piscine
 
 func IsAlpha(str string) bool {
 
-	var type bool 
+	var word bool
 
 	for i := 0; i < len(str); i++ {
-		if 'a' <= str[i] && str[i]  <= 'z' || 'A' <= str[i] && str[i] <= 'Z' || '0' <= str[i] && str[i] <= '9' {
-			type = true
-		}else{
-			type = false
+		if 'a' <= str[i] && str[i] <= 'z' || 'A' <= str[i] && str[i] <= 'Z' || '0' <= str[i] && str[i] <= '9' {
+			word = true
+		} else {
+			word = false
 		}
 	}
-	return type
+	return word
 }
