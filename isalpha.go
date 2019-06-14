1a1bf271ea595b7b35f16d944014b41bbf221f8d
RichK <amosamospascal@yahoo.com>
2019-06-14 19:43:57 +0000

    isprintble

--- isalpha.go
+++ isalpha.go
@@ -1,14 +1,15 @@
 package piscine
 
-func isPrintable(c byte) bool {
-	return 0x20 <= c && c <= 0x7E
-}
+func IsAlpha(str string) bool {
 
-func IsPrintable(s string) bool {
-	for i := 0; i < len(s); i++ {
-		if !isPrintable(s[i]) {
-			return false
+	var word bool
+
+	for i := 0; i < len(str); i++ {
+		if 'a' <= str[i] && str[i] <= 'z' || 'A' <= str[i] && str[i] <= 'Z' || '0' <= str[i] && str[i] <= '9' {
+			word = true
+		} else {
+			word = false
 		}
 	}
-	return true
+	return word
 }
