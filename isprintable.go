debfa82aac6a95405e1f2e0f304b6bcba7d4e504
RichK <amosamospascal@yahoo.com>
2019-06-14 21:03:25 +0000

    isprintble

--- isprintable.go
+++ isprintable.go
@@ -4,7 +4,7 @@ func IsPrintable(str string) bool {
 	b := []byte(str)
 	var t bool
 	for i := 0; i < len(b); i++ {
-		if 65 <= str[i] && str[i] <= 90 || 97 <= str[i] && str[i] <= 122 || 47 <= str[i] && str[i] <= 57 {
+		if 65 <= str[i] && str[i] <= 90 || 97 <= str[i] && str[i] <= 122 || 47 <= str[i] && str[i] <= 57 || str[i] == "!" {
 			return true
 		} else {
 			t = false
