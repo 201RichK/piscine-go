f493f6218a80058747cac6e4820eecaae5d9c516
RichK <amosamospascal@yahoo.com>
2019-06-14 16:36:14 +0000

    virify is somme string is alphanumerique

--- isalpha.go
+++ isalpha.go
@@ -5,7 +5,7 @@ func IsAlpha(str string) bool {
 	var type bool 
 
 	for i := 0; i < len(str); i++ {
-		if 'a' <= str[i] && str[i]  <= 'z' || 'A' <= str[i] && str[i] <= 'Z' || '0' <= str[i] && <= '9' {
+		if 'a' <= str[i] && str[i]  <= 'z' || 'A' <= str[i] && str[i] <= 'Z' || '0' <= str[i] && str[i] <= '9' {
 			type = true
 		}else{
 			type = false
