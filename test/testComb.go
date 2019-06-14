00c75cce327ac90d6c4ae7b70ae02330119c606b
RichK <amosamospascal@yahoo.com>
2019-06-14 21:49:22 +0000

    concat

--- test/testComb.go
+++ test/testComb.go
@@ -4,20 +4,10 @@ import (
 	"fmt"
 )
 
-func IsPrintable(str string) bool {
-	b := []byte(str)
-	var t bool
-	for i := 0; i < len(b); i++ {
-		if 65 <= str[i] && str[i] <= 90 || 97 <= str[i] && str[i] <= 122 || 47 <= str[i] && str[i] <= 57 {
-			return true
-		} else {
-			t = false
-		}
-	}
-	return t
+func Concat(str1 string, str2 string) string {
+	return str1 + str2
 }
 
 func main() {
-	fmt.Println(IsPrintable("01,02,03"))
-	fmt.Println(IsPrintable("\f\t\f\f\t\n\v\a\t\v\t\r\b\r\a\t\t\f\t\t"))
+	fmt.Println(Concat("Hello!", " How are you?"))
 }
