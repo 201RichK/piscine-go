621176572f3637ce06d09ec8208d133a81f625a1
RichK <amosamospascal@yahoo.com>
2019-06-21 00:57:20 +0000

    any

--- any.go
+++ any.go
@@ -2,13 +2,13 @@ package piscine
 
 func Any(f func(string) bool, arr []string) bool {
 
-	var type bool = false
+	var fType bool = false
 
 	for i := 0; i < len(arr); i++ {
 		if f(i) {
-			type = true
+			fType = true
 		}
 	}
 
-	return type
+	return fType
 }
