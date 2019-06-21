1c7a560dce9153e3d9aa8bac89486a300679468c
RichK <amosamospascal@yahoo.com>
2019-06-21 00:44:53 +0000

    map

--- map.go
+++ map.go
@@ -1,7 +1,7 @@
 package piscine
 
 func Map(f func(int) bool, arr []int) []bool {
-	var tbl make([]bool, len(arr))
+	var tbl := make([]bool, len(arr))
 	i := 0
 	for _, elmt := range arr {
 		tbl[i] = f(elmt)
