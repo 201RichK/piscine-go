d762ced8b85a0a317642e2e530135a0850e80516
RichK <amosamospascal@yahoo.com>
2019-06-22 14:56:56 +0000

    rot14

--- rot14.go
+++ rot14.go
@@ -7,7 +7,7 @@ func Rot14(str string) string {
 	for i := 0; i < len(str); i++ {
 		if (newStr[i] >= 'A' && newStr[i] <= 'L') || (newStr[i] >= 'a' && newStr[i] <= 'l') {
 			newStr[i] += 14
-		} else if (str[i] >= 'M' && str[i] <= 'Z') || (str[i] >= 'o' && str[i] <= 'z') {
+		} else if (newStr[i] >= 'M' && newStr[i] <= 'Z') || (newStr[i] >= 'm' && newStr[i] <= 'z') {
 			newStr[i] -= 12
 		}
 	}
