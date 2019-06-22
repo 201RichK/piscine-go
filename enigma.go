f35bbdde4f5ca0010e0a91f7c1b29ddd2fff111e
RichK <amosamospascal@yahoo.com>
2019-06-22 20:52:28 +0000

    enigma

---
+++ enigma.go
@@ -0,0 +1,11 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	valc := *******c
+	*******c = ***a
+	vald := ****d
+	****d = valc
+	valb := *b
+	*b = vald
+	***a = valb
+}
