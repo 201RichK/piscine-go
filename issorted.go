77273d5b786b22e3c507e130c4d4d8719f5270bd
RichK <amosamospascal@yahoo.com>
2019-06-22 09:44:58 +0000

    issorted

--- issorted.go
+++ issorted.go
@@ -3,10 +3,10 @@ package piscine
 func IsSorted(f func(a, b int) int, tab []int) bool {
 	a := true
 	for i := 0; i < len(tab); i++ {
-		if f(tab[i], tab[i+1]) >= 0 {
+		if f(tab[i], tab[i+1]) < 0 {
+			return false
+		} else if f(tab[i], tab[i+1]) >= 0 {
 			a = true
-		} else if f(tab[i], tab[i+1]) < 0 {
-			return a
 		}
 	}
 	return a
