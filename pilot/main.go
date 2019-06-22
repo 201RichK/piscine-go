86bcb1e2a26ee4c1aabb14697c2844dda92be957
RichK <amosamospascal@yahoo.com>
2019-06-22 23:12:04 +0000

    msg

--- pilot/main.go
+++ pilot/main.go
@@ -1,10 +1,22 @@
-package student
+package main
 
-var AIRCRAFT1 = 1
+import "fmt"
+
+const AIRCRAFT1 = 1
 
 type Pilot struct {
 	Name     string
 	Life     int
 	Age      int
 	Aircraft int
+}
+
+func main() {
+	var donnie Pilot
+	donnie.Name = "Donnie"
+	donnie.Life = 100.0
+	donnie.Age = 24
+	donnie.Aircraft = AIRCRAFT1
+
+	fmt.Println(donnie)
 }
