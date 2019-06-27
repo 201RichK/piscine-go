67b2c7804b901e444682207176f3a4ba4e192397
RichK <amosamospascal@yahoo.com>
2019-06-27 01:25:30 +0000

    listpushBack

---
+++ listpushback.go
@@ -0,0 +1,26 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+	} else {
+		listItem := l.Head
+
+
+		for listItem.Next != nil {
+			listItem = listItem.Next
+		}
+
+		listItem.Next = n
+	}
+}
