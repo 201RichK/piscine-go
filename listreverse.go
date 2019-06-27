f4801d1a1deeb0fad050e76377c88b5228d9a070
RichK <amosamospascal@yahoo.com>
2019-06-27 19:15:48 +0000

    reverse list

--- listreverse.go
+++ listreverse.go
@@ -1,19 +1,18 @@
 package piscine
 
 //reverses the list
-package piscine
 
 func ListReverse(l *List) {
-	cl:=&List{}
-	if l.Head!=nil{
-		cl.Tail=l.Head
-		for l.Head.Next!=nil{
+	cl := &List{}
+	if l.Head != nil {
+		cl.Tail = l.Head
+		for l.Head.Next != nil {
 			ListPushFront(cl, l.Head.Data)
-			l.Head=l.Head.Next
+			l.Head = l.Head.Next
 		}
 		ListPushFront(cl, l.Head.Data)
-		l.Head=cl.Head
-		l.Tail=cl.Tail
-		l.Tail.Next=nil
+		l.Head = cl.Head
+		l.Tail = cl.Tail
+		l.Tail.Next = nil
 	}
 }
