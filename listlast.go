f2d93e721a031e262a930b9938de2afbd04bf26d
RichK <amosamospascal@yahoo.com>
2019-06-27 17:20:33 +0000

    listlast

--- listlast.go
+++ listlast.go
@@ -1,15 +1,5 @@
 package piscine
 
-type Node struct {
-	Data interface{}
-	Next *Node
-}
-
-type List struct {
-	Head *Node
-	Tail *Node
-}
-
 func ListLast(l *List) interface{} {
 	if l.Head == nil {
 		return nil
