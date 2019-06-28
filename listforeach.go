2bb2ea1d2f867e74440d7f86de879bace130297e
201RichK <nan@iMac-39.local>
2019-06-28 16:29:44 +0000

    lis for each

--- listforeach.go
+++ listforeach.go
@@ -3,11 +3,14 @@ package piscine
 
 func ListForEach(l *List, f func(*NodeL)) {
 
+	l.Tail = l.Head
 	for  l.Head != nil {
 		f(l.Head)
 		l.Head = l.Head.Next
 	}
-	l.Head = l.Tail
+	temp := l.Head
+    l.Head = l.Tail
+    l.Tail = temp
 }
 
 
