e8e41acb7f561dfa3886ded06c396e638710de5e
RichK <amosamospascal@yahoo.com>
2019-06-28 23:59:53 +0000

    BTreeApplyPostorder

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -1,6 +1,6 @@
 package piscine
 
-func BTreeApplyPostorder(root *piscine.TreeNode, f func(...interface{}) (int, error)) {
+func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root != nil {
 		BTreeApplyPostorder(root.Left, f)
 		BTreeApplyPostorder(root.Right, f)
