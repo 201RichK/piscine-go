b2a68e5448bc1f650f19b165e278ea548385c10c
RichK <amosamospascal@yahoo.com>
2019-06-28 23:14:08 +0000

    sortlistinsert

---
+++ sortlistinsert.go
@@ -0,0 +1,15 @@
+package piscine
+
+func SortListInsert(l *NodeI, data_ref int) *NodeI {
+	n := &NodeI{Data: data_ref}
+	if l == nil {
+		return n
+	}
+	first := l
+	for l.Next != nil {
+		l = l.Next
+	}
+	l.Next = n
+
+	return ListSort(first)
+}
