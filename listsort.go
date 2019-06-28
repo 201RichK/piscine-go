2c80197aa9043737313635d6e23cc8c92c2b0f6b
RichK <amosamospascal@yahoo.com>
2019-06-28 22:58:56 +0000

    listsort

--- listsort.go
+++ listsort.go
@@ -1,15 +1,29 @@
 package piscine
 
-func listSort(l *node) *node {
+func listSort(l *Node) *Node {
 
 	ptr := l
 	if ptr == nil {
 		return nil
 	}
-	ptr.next = listSort(ptr.next)
+	ptr.Next = listSort(ptr.Next)
 
-	if ptr.next != nil && ptr.data > ptr.next.data {
+	if ptr.Next != nil && ptr.data > ptr.Next.data {
 		ptr = move(ptr)
 	}
 	return ptr
+}
+
+func move(l *Node) *Node {
+	p := l
+	n := l.Next
+	ret := n
+
+	for n != nil && l.Data > n.Data {
+		p = n
+		n = n.Next
+	}
+	p.Next = l
+	l.Next = n
+	return ret
 }
