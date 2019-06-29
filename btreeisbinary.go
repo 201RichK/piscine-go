2348c5c678157d0a54dc7eca9ac60efe99a541dd
RichK <amosamospascal@yahoo.com>
2019-06-29 02:39:23 +0000

    t

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -1,10 +1,5 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
-
 func BTreeIsBinary(root *TreeNode) bool {
 	if root == nil {
 		return true
