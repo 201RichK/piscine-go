db1eacb3f06dd0e7ddedff3c935e1ee20997cf16
RichK <amosamospascal@yahoo.com>
2019-06-29 00:21:31 +0000

    BTreeIsBinary

---
+++ btreeisbinary.go
@@ -0,0 +1,11 @@
+package piscine
+
+func BTreeIsBinary(root *TreeNode) bool {
+	if root.Left != nil {
+		return BTreeIsBinary(root.Left) && root.Data > root.Left.Data
+	}
+	if root.Right != nil {
+		return BTreeIsBinary(root.Right) && root.Data < root.Right.Data
+	}
+	return false
+}
