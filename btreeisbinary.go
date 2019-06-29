bc0225e854d1aaa286c1a0fb1a7fadfab0a6852b
RichK <amosamospascal@yahoo.com>
2019-06-29 00:55:08 +0000

    BTreeIsBinary

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -11,7 +11,7 @@ func BTreeIsBinary(root *TreeNode) bool {
 	if root.Right != nil {
 		return BTreeIsBinary(root.Right) && root.Data < root.Right.Data
 	}
-	if BTreeIsBinary(&solutions.TreeNode{Left: (*solutions.TreeNode)(0xc000080420), Right: (*solutions.TreeNode)(0xc000080480), Parent: (*solutions.TreeNode)(nil), Data: "04"}) {
+	if root.Parent == nil {
 		return false
 	}
 	return false
